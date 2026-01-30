03bca73996c317d18c4c82e1306fa74989e9991e
mikey1th <mikey@middlegroundmade.com>
2026-01-30 06:05:54 -0500

    Added index and js files.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,14 @@
+/*Okay, so I know I need tthe code to do a few things to play rock paper scossors:
+1.) Get a random number from computer and assign it  a value of rock paper or scissors
+2.) Prompt human to enter one of those same options
+3.) create a new function that determines which one wins
+*/
+
+
+
+console.log("Rock, Rock, always Rock");
+
+function getComputerChoice(max){
+    let computerChoice= Math.floor(Math.random() * max);
+    let rock= 
+}
